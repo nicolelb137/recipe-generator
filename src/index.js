050cdc7d2648e0c919abb46ad99785ed43b2a520
nicolelb137 <nicolelb137@gmail.com>
2025-02-15 16:08:12 -0500

    added api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: " ",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings:
-      "How about a One-Pan Chicken and Veggies recipe? It's easy, quick, and perfect for a family meal! It requires minimal prep and is healthy too.",
-    autoStart: true,
-    cursor: " ",
-  });
+  let userIngredient = document.querySelector("#user-ingredient");
+  let apiKey = "be641daeo4c4132c8da3cfa3610475tb";
+  let context =
+    "You love to cook.  Please create a delicious and simple recipe that uses the user's ingredient. Format the recipe to include 3 separate sections: Title, Ingredients, Instructions.";
+  let prompt = `User's ingredient: ${userIngredient.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
